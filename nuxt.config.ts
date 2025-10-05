79c34626cfa66f9c4f7bc04d65448b45b984dd35
Asciien <quinchon.raphael.061@gmail.com>
2025-10-06 01:03:48 +0200

    SEO and Google verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,17 @@ import { join } from 'node:path'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    [
+      '@nuxt/sitemap',
+      {
+        hostname: 'https://raphaelquinchon.com',
+        gzip: true
+      }
+    ]
+  ],
   
   app: {
     head: {
@@ -14,7 +24,9 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Raphaël Quinchon – Développeur web full-stack passionné, créant des applications rapides, modernes et élégantes. Découvrez mes projets, compétences et réalisations.' }
+        { name: 'description', content: 'Raphaël Quinchon – Développeur web full-stack passionné, créant des applications rapides, modernes et élégantes. Découvrez mes projets, compétences et réalisations.' },
+        { name: 'author', content: 'Raphaël Quinchon' },
+        { name: 'google-site-verification', content: 'o9OKPBwWZ_1qnwomtxwivzX2s_tYEHQd3OYrHfv6GH0' },
       ]
     }
   },
